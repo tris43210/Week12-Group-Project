e32e11a57c236973a949669f5e2ead001e5a6e9d
Ted Preston <tedpreston29@gmail.com>
2025-11-14 11:49:56 +0000

    clerk set-up

--- folio-app/src/app/layout.js
+++ folio-app/src/app/layout.js
@@ -1,4 +1,20 @@
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+
+import { Limelight } from "next/font/google";
+
+const limelight = Limelight({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -7,8 +23,30 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`antialiased`}>
+          <nav className="flex justify-between items-center p-4 gap-4 h-20 bg-[#2D3333] shadow-lg bg-opacity-65 backdrop-blur-sm">
+            <Link href={"/"} className={`${limelight.className} text-4xl`}>
+              FOLIO
+            </Link>
+            <div className="flex items-center gap-4">
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-2 sm:px-3 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </nav>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
