aa47928a79542658b9541f830f1a09caa6f451fd
tris43210 <tristan.g.king@gmail.com>
2025-11-17 14:21:44 +0000

    styled sign up page

--- folio-app/src/app/artist/create-profile/page.js
+++ folio-app/src/app/artist/create-profile/page.js
@@ -1,38 +1,62 @@
 // New artists enter profile details
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/utils/connect";
-import { redirect, RedirectType } from "next/navigation"
-
+import { redirect, RedirectType } from "next/navigation";
 
 export default async function CreateProfilePage() {
-  const {isAuthenticated, redirectToSignIn, userId } = await auth() 
+  const { isAuthenticated, redirectToSignIn, userId } = await auth();
 
   if (!isAuthenticated) {
-    return redirectToSignIn(); 
+    return redirectToSignIn();
   }
 
   async function handleSubmit(formData) {
-    "use server"
+    "use server";
     const data = Object.fromEntries(formData);
-    const sendToDatabase = await db.query('INSERT INTO artist (name, bio, avatar,clerk_id) VALUES ($1, $2, $3, $4)', [data.name, data.bio, data.avatar, userId]); 
-    redirect('myprofile')
+    const sendToDatabase = await db.query(
+      "INSERT INTO artist (name, bio, avatar,clerk_id) VALUES ($1, $2, $3, $4)",
+      [data.name, data.bio, data.avatar, userId]
+    );
+    redirect("myprofile");
   }
 
   return (
-    <>
-      <form action={handleSubmit}>
-        <label htmlFor="user-name">Username: </label>
-        <input type="text" id="user-name" placeholder="Enter a username" name="name" required></input>
-
-        <label htmlFor="user-bio">Profile Bio: </label>
-        <input type="text" id="user-bio" placeholder="Enter a profile bio" name="bio" required></input>
-
-        <label htmlFor="user-avatar">Avatar Image: </label>
-        <input type="text" id="user-avatar" placeholder="Enter a image URL for your Avatar" name="avatar" required></input>
-
-        <button type="submit">Submit Information</button>
-      </form>
-      <div>The form you type your Name, Bio, Avatar link</div>
-    </>
+    <div className="flex justify-center h-screen items-center">
+      <div className="w-full max-w-xs">
+        <form
+          action={handleSubmit}
+          className="bg-folio-slate shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <label htmlFor="user-name" className="block text-folio-cyan text-sm font-bold mb-2">Username: </label>
+            <input type="text" id="user-name" placeholder="Enter a username" name="name" required className="shadow appearance-none border rounded w-full py-2 px-3 text-folio-cyan leading-tight focus:outline-none focus:shadow-outline"></input>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="user-bio" className="block text-folio-cyan text-sm font-bold mb-2">Profile Bio: </label>
+            <input
+              type="text"
+              id="user-bio"
+              placeholder="Enter a profile bio"
+              name="bio"
+              required
+            className="shadow appearance-none border border-folio-cyan text-folio-cyan rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"></input>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="user-avatar" className="block text-folio-cyan text-sm font-bold mb-2">Avatar Image: </label>
+            <input
+              type="text"
+              id="user-avatar"
+              placeholder="Enter a image URL for your Avatar"
+              name="avatar"
+              required
+              className="shadow appearance-none border border-folio-cyan text-folio-cyan rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            ></input>
+          </div>
+          <div className="flex items-center justify-center">
+            <button type="submit" class="bg-folio-blue hover:bg-folio-cyan text-folio-slate font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit Information</button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
