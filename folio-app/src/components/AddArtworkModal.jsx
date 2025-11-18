2b035a1625a9f246a880b9f8deca24b103231352
Ted Preston <tedpreston29@gmail.com>
2025-11-18 16:11:31 +0000

    Add art modal in own component

---
+++ folio-app/src/components/AddArtworkModal.jsx
@@ -0,0 +1,62 @@
+//Add Artwork Modal
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+export default function AddArtworkModal({ submitArt }) {
+  return (
+    <Dialog>
+      <DialogTrigger>Submit Art...</DialogTrigger>
+      <DialogContent className="bg-folio-slate shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <DialogHeader>
+          <DialogTitle>Submit your art here!</DialogTitle>
+          <DialogDescription>Better be your best work!</DialogDescription>
+        </DialogHeader>
+        <form
+          action={submitArt}
+          className="bg-folio-slate shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <label
+            htmlFor="name"
+            className="block text-folio-cyan text-sm font-bold mb-2"
+          >
+            Artwork Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Artwork Name"
+            required
+            className="shadow appearance-none border border-folio-cyan text-folio-cyan rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          <label
+            htmlFor="img"
+            className="block text-folio-cyan text-sm font-bold mb-2"
+          >
+            Image URL
+          </label>
+          <textarea
+            type="text"
+            id="img"
+            name="img"
+            placeholder="Image URL"
+            required
+            className="shadow appearance-none border border-folio-cyan text-folio-cyan rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+
+          <DialogFooter>
+            <button type="submit">Submit</button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
