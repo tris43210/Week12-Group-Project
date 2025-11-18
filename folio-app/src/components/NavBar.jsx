b405f77d8b22d14acaa4f48ece0d45355d72d85b
MrSteveW <MrSteveW@users.noreply.github.com>
2025-11-18 11:02:32 +0000

    fix getArtist utility

--- folio-app/src/components/NavBar.jsx
+++ folio-app/src/components/NavBar.jsx
@@ -1,21 +1,22 @@
 import { Limelight } from "next/font/google";
 import Link from "next/link";
+import { getArtistInfo } from "@/utils/getArtistInfo";
 
 const limelight = Limelight({
   subsets: ["latin"],
   weight: "400",
 });
 
-export default function NavBar() {
+export default async function NavBar() {
+  const artist = await getArtistInfo();
   return (
-    <div>
-      <nav className="flex items-center p-4 gap-4 h-20 bg-[#2D3333] shadow-lg bg-opacity-65">
-        <h1 className={`${limelight.className} text-4xl`}>FOLIO</h1>
-        <div>
-          <Link href={"/"}>Home | </Link>
-          <Link href={"/artist/myprofile"}>My Profile</Link>
-        </div>
-      </nav>
-    </div>
+    <nav className="flex items-center p-4 gap-4 h-20 bg-[#2D3333] shadow-lg bg-opacity-65">
+      <h1 className={`${limelight.className} text-4xl`}>FOLIO</h1>
+      <div>
+        <Link href={"/"}>Home | </Link>
+        <Link href={"/artist/myprofile"}>My Profile</Link>
+      </div>
+      <div>{artist.name}</div>
+    </nav>
   );
 }
