d5f1107b30ab267d0982ea28b854d770ba9f76a0
anjumatif <shaikhanjum4221@gmail.com>
2025-11-19 13:50:51 +0000

    updatewednesday

--- folio-app/src/app/artist/[id]/page.js
+++ folio-app/src/app/artist/[id]/page.js
@@ -1,8 +1,19 @@
 // Individual artist page
+import { db } from "@/utils/connect";
+
 export default async function ArtistPage({ params }) {
+  const { id } = await params;
+  const data = await db.query(
+    `SELECT *
+      FROM artist WHERE id=$1`,
+    [id]
+  );
+  const artInfo = data.rows[0];
   return (
     <div>
       <p>This is the Page for each artists profile.</p>
+      <h2>{artInfo.name}</h2>
+      <h2>{artInfo.bio}</h2>
     </div>
   );
 }
