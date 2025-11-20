3f9cc620a862f2ad3b14252960c472841286fefc
Ted Preston <tedpreston29@gmail.com>
2025-11-20 12:48:40 +0000

    notfound page styleda and added error.js

--- folio-app/src/app/artist/[id]/not-found.js
+++ folio-app/src/app/artist/[id]/not-found.js
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Woopsie!</h1>
-      <p>Sorry, This Person Does Not Exist!</p>
-      <Link href="/">Back to Home?</Link>
+    <div className=" flex justify-center mt-16">
+      <div className=" bg-folio-slate w-fit rounded-2xl p-2 mb-4">
+        <div className="flex flex-col items-center">
+          <h1>Wuuuuh?</h1>
+          <p>Sorry, This Person Does Not Exist!</p>
+          <Link href="/">Back to Home?</Link>
+        </div>
+      </div>
     </div>
   );
 }
--- folio-app/src/app/artwork/[id]/not-found.js
+++ folio-app/src/app/artwork/[id]/not-found.js
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className=" flex justify-center  bg-folio-slate w-fit rounded-xl p-2 mb-4">
-      <div>
-        <h1>Woopsie!</h1>
-        <p>Sorry, Could Not Find Requested Post</p>
-        <Link href="/">Back to Home?</Link>
+    <div className=" flex justify-center mt-16">
+      <div className=" bg-folio-slate w-fit rounded-2xl p-2 mb-4">
+        <div className="flex flex-col items-center">
+          <h1>Woopsie!</h1>
+          <p>Sorry, Could Not Find Requested Post</p>
+          <Link href="/">Back to Home?</Link>
+        </div>
       </div>
     </div>
   );
